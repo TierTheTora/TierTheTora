49b9d5633ba881e9b6f3db30513330eea13d00c0
TierTheTora <thetora@proton.me>
2026-04-21 20:26:31 +0200

    update

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -1,8 +1,18 @@
 vim.g.mapleader = " "
+vim.g.netrw_winsize = 25
+vim.g.netrw_altv = 1
 
 vim.keymap.set('n', '°', '^', { noremap = true, silent = true })
 vim.keymap.set({'n','x','o'}, 'ö', ':', { noremap = false })
 
-vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
-vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = "Show diagnostic" })
-vim.keymap.set('n', '<leader>m', vim.diagnostic.setloclist, { desc = "Show all diagnostics" })
+vim.keymap.set("n", "<leader>c", vim.cmd.Lexplore)
+vim.keymap.set('n', '<leader>v', ':bd<CR>')
+
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>m', vim.diagnostic.setloclist)
+
+vim.keymap.set('n', '<tab>', '<Cmd>BufferNext<CR>')
+vim.keymap.set('n', '<S-tab>', '<Cmd>BufferPrevious<CR>')
+
+vim.keymap.set('n', '<leader>,', ':enew<CR>')
+vim.keymap.set('n', '<leader>.', '<Cmd>BufferClose<CR>')
